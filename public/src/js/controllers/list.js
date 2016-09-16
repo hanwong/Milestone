561caf226ef47585556da7106e90b38ba4764201
HanWoong Kim <hanationbear@gmail.com>
2016-09-16 19:36:43 +0900

    filter-list flip 효과 및 반응형 이미지 사이즈 수정

--- public/src/js/controllers/list.js
+++ public/src/js/controllers/list.js
@@ -6,85 +6,85 @@
       this.contents = [
         {
           'category': 'branding',
-          'name': 'This is branding.',
-          'description': 'This is description.',
+          'name': 'Milestone UI Kit Branding',
+          'description': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore...',
           'link': 'http://',
           'img': '/public/src/img/portf_item01.jpg'
         },
         {
           'category': 'app',
-          'name': 'This is app.',
-          'description': 'This is description.',
+          'name': 'Milestone UI',
+          'description': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore...',
           'link': 'http://',
           'img': '/public/src/img/portf_item02.jpg'
         },
         {
           'category': 'web',
-          'name': 'This is web.',
-          'description': 'This is description.',
+          'name': 'Yet one project',
+          'description': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore...',
           'link': 'http://',
           'img': '/public/src/img/portf_item03.jpg'
         },
         {
           'category': 'branding',
-          'name': 'This is branding.',
-          'description': 'This is description.',
+          'name': 'Lorem Ipsum Project',
+          'description': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore...',
           'link': 'http://',
           'img': '/public/src/img/portf_item04.jpg'
         },
         {
           'category': 'web',
-          'name': 'This is web.',
-          'description': 'This is description.',
+          'name': 'New App Title',
+          'description': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore...',
           'link': 'http://',
           'img': '/public/src/img/portf_item05.jpg'
         },
         {
           'category': 'app',
-          'name': 'This is app.',
-          'description': 'This is description.',
+          'name': 'Duis aute irure dolo',
+          'description': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore...',
           'link': 'http://',
           'img': '/public/src/img/portf_item06.jpg'
         },
         {
           'category': 'web',
-          'name': 'This is web.',
-          'description': 'This is description.',
+          'name': 'Excepteur sint occaecat',
+          'description': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore...',
           'link': 'http://',
           'img': '/public/src/img/portf_item07.jpg'
         },
         {
           'category': 'web',
-          'name': 'This is web.',
-          'description': 'This is description.',
+          'name': 'Llamco laboris nisi ut ali',
+          'description': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore...',
           'link': 'http://',
           'img': '/public/src/img/portf_item08.jpg'
         },
         {
           'category': 'branding',
-          'name': 'This is branding.',
-          'description': 'This is description.',
+          'name': 'Duis aute irure!',
+          'description': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore...',
           'link': 'http://',
           'img': '/public/src/img/portf_item09.jpg'
         },
         {
           'category': 'app',
-          'name': 'This is app.',
-          'description': 'This is description.',
+          'name': 'Lorem ipsum dolor sit amet',
+          'description': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore...',
           'link': 'http://',
           'img': '/public/src/img/portf_item10.jpg'
         },
         {
           'category': 'web',
-          'name': 'This is web.',
-          'description': 'This is description.',
+          'name': 'Excepteur sint',
+          'description': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore...',
           'link': 'http://',
           'img': '/public/src/img/portf_item11.jpg'
         },
         {
           'category': 'web',
-          'name': 'This is web.',
-          'description': 'This is description.',
+          'name': 'Some new proj',
+          'description': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore...',
           'link': 'http://',
           'img': '/public/src/img/portf_item12.jpg'
         }        
