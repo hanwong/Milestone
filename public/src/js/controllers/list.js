059f209cad8592c380b19e8db0161cc4c1c91635
HanWoong Kim <hanationbear@gmail.com>
2016-09-16 15:54:15 +0900

    filter-list 컨텐츠 구성 - hover effect 추가 수정 필요함. - 카드 뒷면 컨텐츠 설정 필요함.

--- public/src/js/controllers/list.js
+++ public/src/js/controllers/list.js
@@ -9,88 +9,91 @@
           'name': 'This is branding.',
           'description': 'This is description.',
           'link': 'http://',
-          'img': 'img.png'
+          'img': '/public/src/img/portf_item01.jpg'
         },
         {
           'category': 'app',
           'name': 'This is app.',
           'description': 'This is description.',
           'link': 'http://',
-          'img': 'img.png'
+          'img': '/public/src/img/portf_item02.jpg'
         },
         {
           'category': 'web',
           'name': 'This is web.',
           'description': 'This is description.',
           'link': 'http://',
-          'img': 'img.png'
+          'img': '/public/src/img/portf_item03.jpg'
         },
         {
           'category': 'branding',
           'name': 'This is branding.',
           'description': 'This is description.',
           'link': 'http://',
-          'img': 'img.png'
+          'img': '/public/src/img/portf_item04.jpg'
+        },
+        {
+          'category': 'web',
+          'name': 'This is web.',
+          'description': 'This is description.',
+          'link': 'http://',
+          'img': '/public/src/img/portf_item05.jpg'
         },
         {
           'category': 'app',
           'name': 'This is app.',
           'description': 'This is description.',
           'link': 'http://',
-          'img': 'img.png'
-        },
-        {
-          'category': 'branding',
-          'name': 'This is branding.',
-          'description': 'This is description.',
-          'link': 'http://',
-          'img': 'img.png'
-        },
-        {
-          'category': 'branding',
-          'name': 'This is branding.',
-          'description': 'This is description.',
-          'link': 'http://',
-          'img': 'img.png'
+          'img': '/public/src/img/portf_item06.jpg'
         },
         {
           'category': 'web',
           'name': 'This is web.',
           'description': 'This is description.',
           'link': 'http://',
-          'img': 'img.png'
+          'img': '/public/src/img/portf_item07.jpg'
         },
         {
           'category': 'web',
           'name': 'This is web.',
           'description': 'This is description.',
           'link': 'http://',
-          'img': 'img.png'
+          'img': '/public/src/img/portf_item08.jpg'
+        },
+        {
+          'category': 'branding',
+          'name': 'This is branding.',
+          'description': 'This is description.',
+          'link': 'http://',
+          'img': '/public/src/img/portf_item09.jpg'
         },
         {
           'category': 'app',
           'name': 'This is app.',
           'description': 'This is description.',
           'link': 'http://',
-          'img': 'img.png'
+          'img': '/public/src/img/portf_item10.jpg'
         },
         {
           'category': 'web',
           'name': 'This is web.',
           'description': 'This is description.',
           'link': 'http://',
-          'img': 'img.png'
+          'img': '/public/src/img/portf_item11.jpg'
         },
         {
           'category': 'web',
           'name': 'This is web.',
           'description': 'This is description.',
           'link': 'http://',
-          'img': 'img.png'
+          'img': '/public/src/img/portf_item12.jpg'
         }        
       ];
 
+
     });
+
+  
 
 
 //END IIFE PATTERN
