d4848f72146f3e6559dc26511a48f82a65982af9
HanWoong Kim <hanationbear@gmail.com>
2016-09-20 19:11:59 +0900

    모바일 브라우저 시에  호버효과 삭제 및 터치 효과 추가

--- public/src/js/response.js
+++ public/src/js/response.js
@@ -4,10 +4,34 @@
   var $html = $('html');
   // js 설정 가는 환경 체크 
   $('html').removeClass('no-js').addClass('js');
-  $(window).on('load resize', {'i': 0}, function(event) {
+  $(global).on('load resize', {'i': 0}, function(event) {
     changeDeviceMode($html, event.data.i++ === 0 ? true : false);
   });
 
+  var touchsupport = ('ontouchstart' in window) || (navigator.maxTouchPoints > 0) || (navigator.msMaxTouchPoints > 0)
+  if (!touchsupport){ // browser doesn't support touch
+      document.documentElement.className += " non-touch"
+  }
+
+  $(function(){
+    
+    var $card = $('.element-item');
+    
+    $card.each(function(index, el) {
+      $card.eq(index).on('touchstart', function (e) {
+        if ( !$card.eq(index).hasClass('hover') ) {
+          $card.eq(index).addClass('hover');
+          // TweenMax.to($card.eq(index), 0.5, { 'transform': translateY(-3px), 'transform': rotateY( 180deg ) });
+        } 
+        else {
+          $card.eq(index).removeClass('hover');
+          // TweenMax.to($card.eq(index), 0.5, { 'transform': rotateY( 180deg ) , 'transform': translateY( 0 )});
+        }
+
+      });
+    });
+
+  });
+  
 //END IIFE PATTERN
 })(this, this.jQuery);
-  
